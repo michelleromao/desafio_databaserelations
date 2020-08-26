a5ba176c2d06b1758cba752df5d67439532c3dca
Michelle Romão <michelle.nunes10@gmail.com>
2020-08-25 22:24:31 -0300

    Desafio database relations

--- src/modules/customers/infra/http/controller/CustomersController.ts
+++ src/modules/customers/infra/http/controller/CustomersController.ts
@@ -6,6 +6,12 @@ import { container } from 'tsyringe';
 
 export default class CustomersController {
   public async create(request: Request, response: Response): Promise<Response> {
-    // TODO
+    const { name, email } = request.body;
+
+    const createConsumer = container.resolve(CreateCustomerService);
+
+    const consumer = await createConsumer.execute({ name, email });
+
+    return response.json(consumer);
   }
 }
